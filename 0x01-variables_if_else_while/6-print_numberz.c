bde38d3ae8b7b983943b57d7dda93e341a9822a9
Idoko Attah <idoko_research@outlook.com>
2023-06-18 04:39:34 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program to print lowercase alphabet
+ *
+ * Return: Always o (Success!)
+ */
+
+int main(void)
+{
+	int num = 48;
+
+	for (num = 48; num <= 57; num++)
+	{
+		putchar(num);
+		/* putchar('\n');*/
+	}
+	putchar('\n');
+	return (0);
+}
