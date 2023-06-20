06f25373ca5e96ff7a274fdfd1f873c49f466d39
Idoko Attah <idoko_research@outlook.com>
2023-06-20 16:23:23 -0700

    Absolute value task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - function that computes the absolute value of an integer
+ * @n: is the integer whose sign is to be printed
+ * written by @a_idk
+ * Return: Always 0 (Success!)
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
