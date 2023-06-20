137a23bea81436b3c27c8887276d70379d0003ed
Idoko Attah <idoko_research@outlook.com>
2023-06-20 14:19:36 -0700

    Print lowercase Alphabet task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - function that prints the alphabet in lowercase
+ * written by @a_idk
+ * Return: Always 0 (Success!)
+ */
+void print_alphabet(void)
+{
+	int alp;
+
+	for (alp = 97; alp <= 122; alp++)
+	{
+		_putchar(alp);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - function that prints the alphabet in lowercase
+ * written bt @a_idk
+ * Return: Always 0 (Success!)
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		_putchar(alp);
+
+	_putchar('\n');
+}
