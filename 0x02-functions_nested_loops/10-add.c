6de0fa3ef7367701c6f4bee0839802dccd7f5857
Idoko Attah <idoko_research@outlook.com>
2023-06-20 17:53:02 -0700

    Sum of integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - function that adds 2 integers and returns the result
+ * @m: the first of the two integers to be added
+ * @n: the second of the two integers to be added
+ * written by @a_idk
+ * Return: sum of the two integers
+ */
+int add(int m, int n)
+{
+	int sum = m + n;
+
+	return (sum);
+}
