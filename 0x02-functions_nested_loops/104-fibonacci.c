fc77d0742a630ab491c1b5eeef054276c1abf930
Idoko Attah <idoko_research@outlook.com>
2023-06-21 06:20:11 -0700

    First 98 Fibonacci Numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,65 +9,42 @@
  */
 int main(void)
 {
-	unsigned long int a, prev = 1, prev1, prev11,  next = 2;
-	unsigned long int next1, next11, sum = 0;
-	int a;
-	unsigned long int b = 1;
-	unsigned long int c = 2;
-	unsigned long int new;
+	int tally, l = 1000000000;
+	int d = l - 1;
+	unsigned long int prev = 0, prev_a, prev_b,  next = 1;
+	unsigned long int next_a, next_b, sum = 0, a, b, new;
 
 	/* Begin for loop */
-
-	unsigned long int i;
-		unsigned long int bef = 1;
-		unsigned long int aft = 2;
-		unsigned long int l = 1000000000;
-		unsigned long int bef1;
-		unsigned long int bef2;
-		unsigned long int aft1;
-		unsigned long int aft2;
-
-
-		printf("%lu", bef);
-
-
-		for (i = 1; i < 91; i++)
-		{
-			printf(", %lu", aft);
-			aft += bef;
-			bef = aft - bef;
-		}
-
-
-		bef1 = (bef / l);
-		bef2 = (bef % l);
-		aft1 = (aft / l);
-		aft2 = (aft % l);
-
-
-		for (i = 92; i < 99; ++i)
-		{
-			printf(", %lu", aft1 + (aft2 / l));
-			printf("%lu", aft2 % l);
-			aft1 = aft1 + bef1;
-			bef1 = aft1 - bef1;
-			aft2 = aft2 + bef2;
-			bef2 = aft2 - bef2;
-		}
-		printf("\n");
-		return (0);
-	}
-
-	for (a = 1; a <= 33; ++a)
+	for (tally = 0; tally < 92; tally++)
 	{
-		if (((b % 2) == 0) && (b < 4000000))
-		{
-			sum = sum + b;
-		}
-		new = b + c;
-		b = c;
-		c = new;
+		new = prev + next;
+		printf("%lu, ", new);
+		prev = next;
+		next = new;
 	}
-	printf("%lu\n", sum);
+	prev_a = prev / l;
+	next_a = next / l;
+	prev_b = prev % l;
+	next_b = next % l;
+	for (tally = 93; tally < 99; tally++)
+	{
+		a = prev_a + next_a;
+		b = prev_b + next_b;
+		if ((prev_b + next_b) > d)
+		{
+			a += 1;
+			b %= l;
+		}
+		printf("%lu%lu", a, b);
+		if (tally != 98)
+		{
+			printf(", ");
+		}
+		prev_a = next_a;
+		prev_b = next_b;
+		next_a = a;
+		next_b = b;
+	}
+	printf("\n");
 	return (0);
 }
