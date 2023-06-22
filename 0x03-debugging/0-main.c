8b3654a9c7542a2ab264a08db52dfbb3a9d7adf6
Idoko Attah <idoko_research@outlook.com>
2023-06-22 01:34:02 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
 #include "main.h"
 
 /**
- * main - function that prints/test if integer is +ve or -ve
- *
- * Return: Always 0 (Success!)
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
-	positive_or_negative(i);
+        i = 0;
+        positive_or_negative(i);
 
-	return (0);
+        return (0);
 }
