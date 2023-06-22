a9f2e01ef82fde19bc8bf7071c3937fc29a7a285
Idoko Attah <idoko_research@outlook.com>
2023-06-22 10:31:41 -0700

    Second part of task

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -19,19 +19,23 @@ int main(void)
 	{
 		if ((n % 3 == 0) && (n % 5 != 0))
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else if ((n % 3 != 0) && (n % 5 == 0))
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
+		}
+		else if (n == 1)
+		{
+			printf("%d", n);
 		}
 		else
 		{
-			printf("%d ", n);
+			printf(" %d", n);
 		}
 	}
 	printf("\n");
