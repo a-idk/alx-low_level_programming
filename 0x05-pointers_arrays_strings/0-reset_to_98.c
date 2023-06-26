33e3b1dbf8d7e58de666c35a2877ad0bda8a5433
Idoko Attah <idoko_research@outlook.com>
2023-06-26 13:45:48 -0700

    Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+/* include header files */
+#include "main.h"
+
+/**
+ * reset_to_98 - function that takes a pointer to an int & updates to 98
+ * @n: input
+ * @a_idk
+ * Return: null
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
