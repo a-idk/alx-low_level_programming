39f6b8921026f1ef750498208462f87f8477c0b8
Idoko Attah <idoko_research@outlook.com>
2023-07-03 18:20:46 -0700

    Advanced task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+/* include header files */
+#include "main.h"
+
+/**
+ * set_string - function that sets the value of a pointer to a char.
+ * @s: pointer to pointer variable
+ * @to: pointer char
+ * @a_idk
+ * Return: VOID
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
