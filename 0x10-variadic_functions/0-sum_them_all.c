846fc14ed0c7479860014cf13670cc1c04a9a363
Idoko Attah <idoko_research@outlook.com>
2023-07-20 18:26:35 -0700

    Sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -23,7 +23,7 @@ int sum_them_all(const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		result = result + va_arg(x, int); /* retrieve an argument */
+		result += va_arg(x, int); /* retrieve an argument */
 	}
 	/* freeing the va_list x */
 	va_end(x);
