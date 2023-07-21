8eed45aad20425c9df5a6930297ac118b0151c25
Idoko Attah <idoko_research@outlook.com>
2023-07-20 20:01:20 -0700

    update to task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,18 +3,6 @@
 
 #include <stdarg.h>
 
-/**
- * struct prints - defining a new struct type prints
- * @symb: symbol representing the data type
- * @print: pointer to a function that prints data type according to the symbol
- * @a_idk
- */
-
-typedef struct prints
-{
-	char *symb;
-	void(*print)(va_list x);
-} prints_t;
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
