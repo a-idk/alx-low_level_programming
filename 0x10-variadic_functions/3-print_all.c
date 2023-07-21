e98b941351822cdb5e114b57d6fc4a9dd0d49fe6
Idoko Attah <idoko_research@outlook.com>
2023-07-20 19:35:01 -0700

    Print Anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,126 +2,52 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-
-/* declaring functions */
-void print_char(va_list n);
-void print_nums(va_list ar_c);
-void print_flt(va_list ar_c);
-void print_str(va_list ar_c);
-void print_all(const char * const format, ...);
-
-/**
- * print_x - function that prints a character
- * @n: argument list pointing to the character to be printed.
- * @a_idk
- *
- * Return: Nada!
- */
-
-void print_char(va_list n)
-{
-	/* declaring variable */
-	char c;
-
-	c = va_arg(n, int); /* retrieving argument */
-	printf("%c", c); /* print char */
-}
-
-/**
- * print_nums - function that prints an int/num
- * @ar_c: list of arguments pointing to the integer
- *@a_idk
- *
- * Return: Nada!
- */
-
-void print_nums(va_list ar_c)
-{
-	/* declaring variable */
-	int n;
-
-	n = va_arg(ar_c, int); /* retrieving argument */
-	printf("%d", n); /* print integer */
-}
-
-/**
- * print_flt - function that prints a float
- * @ar_c: list of arguments pointing to the float
- * @a_idk
- *
- * Return: Nada!
- */
-
-void print_flt(va_list ar_c)
-{
-	/* declaring variable */
-	float f;
-
-	f = va_arg(ar_c, double); /* retrieving argument */
-	printf("%f", f); /* prints float */
-}
-
-/**
- * print_str - fuction that prints string
- * @ar_c: list of arguments pointing to the string
- * @a_idk
- *
- * Return: Nada!
- */
-
-void print_str(va_list ar_c)
-{
-	/* variable declaration */
-	char *str;
-
-	str = va_arg(ar_c, char *); /* retrieving argument */
-	if (str == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", str);
-}
-
 /**
  * print_all - function that prints anything
  * @format: the fixed argument
  * @a_idk
- *
  * Return: Nada!
  */
-
 void print_all(const char * const format, ...)
 {
-	/* variables declaration and initialization */
-	int index1 = 0;
-	int index2 = 0;
-	prints_t opers[] = {
-		{"c", print_char},
-		{"i", print_nums},
-		{"f", print_flt},
-		{"s", print_str}
-	};
-	va_list x;
-	char *separator = "";
+	/* declaring and initializing variables */
+	unsigned int count = 0, index1 = 0, index2;
+	const char t_format[] = "cifs";
+	char *s;
+	va_list xlist;
 
-	/* starting the iterating arguments of va_list x */
-	va_start(x, format);
-
-	while (format && (*(format + index1)))
+	/* starting the iterative argument of va_list xlist */
+	va_start(xlist, format);
+	while (format && format[index1])
 	{
-		while ((index2 < 4) && (*(format + index1) != *(opers[index2].symb)))
+		index2 = 0;
+		while (t_format[index2])
 		{
-			index2++;
+			if (format[index1] == t_format[index2] && count)
+			{
+				printf(", ");
+				break;
+			} index2++;
 		}
-		if (index2 < 4)
+		switch (format[index1])
 		{
-			printf("%s", separator);
-			opers[index2].print(x);
-			separator = ", ";
-		}
-		index1 += 1;
-	}
-	printf("\n");
-	va_end(x); /* freeing the va_list x */
+			case 'c':
+				printf("%c", va_arg(xlist, int)), count = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(xlist, int)), count = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(xlist, double)), count = 1;
+				break;
+			case 's':
+				s = va_arg(xlist, char *), count = 1;
+				if (!s)
+				{
+					printf("(nil)");
+					break;
+				} printf("%s", s);
+				break;
+		} index1++;
+	} printf("\n"), va_end(xlist); /* free va_list xlist */
 }
