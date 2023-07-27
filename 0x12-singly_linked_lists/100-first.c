ce0631d69e5d792492777fd931c3201e5d3ceb0b
Idoko Attah <idoko_research@outlook.com>
2023-07-27 15:54:35 -0700

    Advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,14 @@
+/* Title: 100-first.c */
+/* include header file */
 #include <stdio.h>
 
 void first(void) __attribute__ ((constructor));
 
 /**
- * first - prints a sentence before the main
- * function is executed
+ * first - function that prints a sentence before the main
+ * @a_idk
  */
+
 void first(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
