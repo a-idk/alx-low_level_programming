85110d27126123f713c96eeb4a7de1f28a2af9c1
Idoko Attah <idoko_research@outlook.com>
2023-08-03 08:43:48 -0700

    Endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+/* Title: 100-get_endianness.c */
+/* include header file */
+#include "main.h"
+
+/**
+ * get_endianness - function that checks the endianness
+ * @a_idk
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	/* declaring and assigning variables */
+	char *ch;
+	unsigned int k = 1;
+
+	ch = (char *) &k;
+	return (*ch);
+}
