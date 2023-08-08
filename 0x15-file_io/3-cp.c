b1dcc6f785f60677c679fe21c8de7b16609f905b
Idoko Attah <idoko_research@outlook.com>
2023-08-07 17:35:01 -0700

    second update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,112 +1,99 @@
 /* Title: 3-cp.c */
 
-/* include header file */
+/* include header files */
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+char *create_buffer(char *file);
+void close_file(int fd);
+
 /**
- * cannot_read - function that checks for error 98 - cannot read
- * @file1: the file to check for error
- * @buff: the value of the buffer
- * @argv: argument vector
+ * error_100 - function that closes file descriptors
+ * @fd: The file descriptor to be closed
  * @a_idk
  *
  * Return: Nada!
  */
 
-void cannot_read(int file1, char *buff, char *argv)
+void error_100(int fd)
 {
-	if (file1 < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: can't read from the file %s\n", argv);
-		free(buff);
-		exit(98);
-	}
+	/* declare variables */
+	int count;
+
+	count = close(fd);
+	if (count == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
 }
 
-
 /**
- * cannot_write - function that checks for error 99 - cannot read
- * @file1: the file to check for error
- * @buff: the value of the buffer
- * @argv: argument vector
+ * bytes_to__buff - function that allocates 1024 bytes for a buffer
+ * @f: The name of the file buffer is storing
  * @a_idk
  *
- * Return: Nada!
+ * Return: char pointer to the newly-allocated buffer
  */
 
-void cannot_write(int file1, char *buff, char *argv)
+char *bytes_to_buff(char *f)
 {
-	if (file1 < 0)
+	/* declare variable */
+	char *buff;
+
+	/* allocate dynamic memory */
+	buff = malloc(sizeof(char) * 1024);
+
+	/* check for Null */
+	if (buff == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv);
-		free(buff), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", f);
+		exit(99);
 	}
+	return (buff);
 }
 
 /**
- * cannot_close - function that checks for error 100 - cannot read
- * @file1: the file to check for error
- * @buff: the value of the buffer
- * @argv argument vector
+ * main - program that copies the contents of a file to another file
+ * @argc: The number of arguments supplied
+ * @argv: An array of pointers to the arguments (vector)
  * @a_idk
  *
- * Return: Nada!
- */
-
-void cannot_close(int file1, char *buff)
-{
-	if (file1 < 0)
-	{
-		dprintf(STDERR_FILENO, "Error: can't close fd %i\n", file1);
-		free(buff), exit(100);
-	}
-}
-
-/**
- * main - program that copies the content of a file to another file
- * @argc: The number of arguments vector
- * @argv: pointer to the array of the arguments
- * @a_idk
- *
- * Return: int 0 on success
+ * Return: 0 on success.
  */
 
 int main(int argc, char *argv[])
 {
-	/* declaring variables | fd = open, rd = read, wr = write */
-	char *buff1;
-	int fd, check1, check2, f;
+	/* declaring variable */
+	char *buf;
+	int file1, file2, rd, wr;
 
-	/* ensuring 2 arguments per time */
+	/* checking of nos of arguments */
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	/* dynamic memory allocation */
-	buff1 = malloc(sizeof(char) * BUFF_SIZE);
-	if (!buff1)
-	{
-		return (0);
-	}
-	/* file operation */
-	fd = open(argv[1], O_RDONLY);
-	cannot_read(fd, buff1, argv[1]);
-	f = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	cannot_write(f, buff1, argv[2]);
 
-	while (check2 >= BUFF_SIZE)
-	{
-		check1 = read(fd, buff1, BUFF_SIZE);
-		if (check1 == 0)
-			break;
-		cannot_read(check1, buff1, argv[1]);
-		check2 = write(f, buff1, check1);
-		cannot_write(check2, buff1, argv[2]);
-	}
-	check1 = close(f);
-	cannot_close(check1, buff1);
-	check1 = close(fd);
-	cannot_close(check1, buff1);
-	free(buff1);
+	/* file operations - read */
+	buf = bytes_to_buff(argv[2]);
+	file1 = open(argv[1], O_RDONLY);
+	rd = read(file1, buf, 1024);
+	file2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	do {
+		if (rd == -1 || file1 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			free(buf);
+			exit(98);
+		}
+		/* file operations - write */
+		wr = write(file2, buf, rd);
+		if (file2 == -1 || wr == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			free(buf), exit(99);
+		}
+		rd = read(file1, buf, 1024);
+		file2 = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (rd > 0);
+	free(buf), error_100(file1), error_100(file2);
 	return (0);
 }
