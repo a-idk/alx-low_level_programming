4d3b04f19e2d1f1d8f92b110003e6abee4819452
a-idk <idoko_research@outlookcom>
2023-09-04 15:09:52 -0700

    task update

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /* Prototypes */
 ssize_t read_textfile(const char *filename, size_t letters);
