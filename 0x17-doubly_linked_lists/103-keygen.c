6bf3ea7ef3c03c4bce8b9ddf7721444237244216
a-idk <idoko_research@outlookcom>
2023-10-12 16:11:43 -0700

    keygen

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,53 +1,52 @@
+/* Title: 103-keygen.c */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * main - Generates and prints passwords for the crackme5 executable.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * main - function that generates and prints passwords
+ * for the crackme5 executable.
+ * @argc: argument count
+ * @argv: argument vector
+ * @a_idk coding
  *
- * Return: Always 0.
+ * Return: Always 0 on Success!
  */
+
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	char password[7], *codex;
-	int len = strlen(argv[1]), i, tmp;
+	/* initializing variables */
+	int idx;
+	char password[7];
+	char *code;
+	int len = strlen(argv[1]);
+	int temp;
 
-	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-
-	tmp = (len ^ 59) & 63;
-	password[0] = codex[tmp];
-
-	tmp = 0;
-	for (i = 0; i < len; i++)
-		tmp += argv[1][i];
-	password[1] = codex[(tmp ^ 79) & 63];
-
-	tmp = 1;
-	for (i = 0; i < len; i++)
-		tmp *= argv[1][i];
-	password[2] = codex[(tmp ^ 85) & 63];
-
-	tmp = 0;
-	for (i = 0; i < len; i++)
+	code = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	temp = (len ^ 59) & 63, password[0] = code[temp];
+	temp = 0; /* for temp = 0 */
+	for (idx = 0; idx < len; idx += 1)
+		temp = temp + argv[1][idx];
+	password[1] = code[(temp ^ 79) & 63];
+	temp = 1; /* for temp = 1 */
+	for (idx = 0; idx < len; idx += 1)
+		temp = temp * argv[1][idx];
+	password[2] = code[(temp ^ 85) & 63];
+	temp = 0; /* setting tempt to 0 */
+	for (idx = 0; idx < len; idx += 1)
 	{
-		if (argv[1][i] > tmp)
-			tmp = argv[1][i];
+		if (argv[1][idx] > temp)
+			temp = argv[1][idx];
 	}
-	srand(tmp ^ 14);
-	password[3] = codex[rand() & 63];
-
-	tmp = 0;
-	for (i = 0; i < len; i++)
-		tmp += (argv[1][i] * argv[1][i]);
-	password[4] = codex[(tmp ^ 239) & 63];
-
-	for (i = 0; i < argv[1][0]; i++)
-		tmp = rand();
-	password[5] = codex[(tmp ^ 229) & 63];
-
-	password[6] = '\0';
+	srand(temp ^ 14), password[3] = code[rand() & 63];
+	temp = 0; /* setting temp to 0 */
+	for (idx = 0; idx < len; idx += 1)
+		temp = temp + (argv[1][idx] * argv[1][idx]);
+	password[4] = code[(temp ^ 239) & 63];
+	for (idx = 0; idx < argv[1][0]; idx += 1)
+		temp = rand();
+	password[5] = code[(temp ^ 229) & 63], password[6] = '\0';
 	printf("%s", password);
 	return (0);
 }
