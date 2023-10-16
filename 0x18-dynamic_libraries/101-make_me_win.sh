4b6c097ade7ae4fadc873d152c0084663fccd668
a-idk <idoko_research@outlookcom>
2023-10-16 15:30:30 -0700

    code injection final update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/a-idk/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD=/tmp/libgiga.so
+gcc -shared -o printer.so -fPIC gamer.c
+export LD_PRELOAD=./printer.so:$LD_LIBRARY_PATH
