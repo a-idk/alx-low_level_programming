8cd5aad8bb8147947b97776f6b75a502786770b9
a-idk <idoko_research@outlook.com>
2023-12-21 01:14:02 +0100

    mandatory task

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -22,9 +22,21 @@ def island_perimeter(grid):
             - Grid is completely surrounded by water (1 island or nothing)
             - The island doesn’t have “lakes”.
     """
+    result = 0  # setting the initial value of perimeter to zero
+
+    for idx in range(len(grid)):
+        for idx2 in range(len(grid[idx])):
+            if grid[idx][idx2] == 1:
+                result = result + 4
+                if ((idx > 0) and (grid[idx - 1][idx2] == 1)):
+                    result = result - 2
+                if ((idx > 0) and (grid[idx][idx2 - 1] == 1)):
+                    result = result - 2
+    return result
+    """
     perimeter = 0
-    flat = []  # flatten 2D grid to 1D list
-    # count = 0
+    # flat = []  # flatten 2D grid to 1D list
+    count = 0
 
     for idx in range(len(grid)):
         for idx2 in range(len(grid[idx])):
@@ -34,7 +46,8 @@ def island_perimeter(grid):
 
     for idx in range(len(flat)):
         if (idx > 0) and (idx < len(flat) - 1):
-            if flat[idx] == 0 and flat[idx + 1] == 1 or flat[idx] == 0 and flat[idx - 1] == 1:
+            if flat[idx] == 0 and flat[idx + 1] == 1 or
+            flat[idx] == 0 and flat[idx - 1] == 1:
             #if (flat[idx] == 0 and flat[idx - 1] == 1):
             #    perimeter = perimeter + 1
             #elif (flat[idx] == 0 and flat[idx + 1] == 1):
@@ -44,4 +57,4 @@ def island_perimeter(grid):
         perimeter = perimeter * 2
     else:
         perimeter = (2 * perimeter) - 1
-    return perimeter
+    return perimeter"""
